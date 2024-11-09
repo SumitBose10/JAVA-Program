dd13fc7d8cadd03330f960614202d6f674e6b897
SumitBose10 <sumitbose260@gmail.com>
2024-11-09 12:56:52 +0530

    bruteforcing example with java

--- Array.java
+++ Array.java
@@ -1,5 +1,34 @@
-public class Array {
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Array {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] arr = { 2, 0, 0, 5 };
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size = sc.nextInt();
+        int[] pass = new int[size];
+        for (int i = 0; i < pass.length; i++) {
+            pass[i] = (int) (Math.random() * 10);
+        }
+        while (true) {
+            try {
+                if (Arrays.equals(arr, pass)) {
+                    System.out.println("Access granted");
+                } else {
+                    throw new PatternNotMatchException("Acesss denied");
+                }
+            } catch (PatternNotMatchException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        sc.close();
+    }
+
+}
+
+class PatternNotMatchException extends Exception {
+    PatternNotMatchException(String message) {
+        super(message);
     }
 }
--- loops.java
+++ loops.java
@@ -1,46 +1,52 @@
-/*import java.util.Scanner;
-
-class loops{
-    public static void main(String Args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = n; i >= 1; i--){
-            for (int j = 1 ; j <= i ; j++){
-                System.err.print("*");
-            }
-            System.out.println();
-        }
-    }
-}*/
+/*
+ * import java.util.Scanner;
+ * 
+ * class loops{
+ * public static void main(String Args[]){
+ * Scanner sc = new Scanner(System.in);
+ * int n = sc.nextInt();
+ * for (int i = n; i >= 1; i--){
+ * for (int j = 1 ; j <= i ; j++){
+ * System.err.print("*");
+ * }
+ * System.out.println();
+ * }
+ * }
+ * }
+ */
 // number pattern
-/*import java.util.Scanner;
-
-class loops{
-    public static void main(String Args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++){
-            for (int j = 1 ; j <= i ; j++){
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-    }
-}*/
+/*
+ * import java.util.Scanner;
+ * 
+ * class loops{
+ * public static void main(String Args[]){
+ * Scanner sc = new Scanner(System.in);
+ * int n = sc.nextInt();
+ * for (int i = 1; i <= n; i++){
+ * for (int j = 1 ; j <= i ; j++){
+ * System.out.print(j);
+ * }
+ * System.out.println();
+ * }
+ * }
+ * }
+ */
 // print number pattern
-/*import java.util.Scanner;
-
-class loops{
-    public static void main(String Args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int number = 1;
-        for (int i = 1; i <= n; i++){
-            for (int j = 1 ; j <= i ; j++){
-                System.out.print(number);
-                number++;
-            }
-            System.out.println();
-        }
-    }
-}*/
+/*
+ * import java.util.Scanner;
+ * 
+ * class loops{
+ * public static void main(String Args[]){
+ * Scanner sc = new Scanner(System.in);
+ * int n = sc.nextInt();
+ * int number = 1;
+ * for (int i = 1; i <= n; i++){
+ * for (int j = 1 ; j <= i ; j++){
+ * System.out.print(number);
+ * number++;
+ * }
+ * System.out.println();
+ * }
+ * }
+ * }
+ */
